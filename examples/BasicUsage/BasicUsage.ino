8bf6e208edc65e57a4907073dfc0006abece38da
per1234 <accounts@perglass.com>
2019-01-12 15:17:41 -0800

    Add mouse position display program
    This HTML+JavaScript program will work on any OS. The previously recommended software can no longer be found so a replacement was needed anyway.
    
    Closes https://github.com/per1234/MouseTo/issues/3

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -1,6 +1,6 @@
 // Demonstrates usage of MouseTo library(http://github.com/per1234/MouseTo).
 // Connect pin 10 to ground to start mouse movements between (0, 0) and another point, which will probably not be (450, 0).
-// Screen coordinates can be determined with this Windows program: http://download.cnet.com/Cursor-Position/3000-2383_4-75449858.html
+// Screen coordinates can be determined by opening the file extras/MousePosition.html included this library in your browser, then press F11 for full screen
 // To determine the correction factor, divide 450(the target x coordinate) by the actual screen x coordinate reached and use the result with MouseTo.setCorrectionFactor().
 // Some additional adjustment of the correction factor may be required to get accurate mouse pointer positioning.
 // Setting a larger target coordinate will yield a more accurate correction factor.
